c0855dcd95c8d9e4ae5d0a2fd612fc75db720d00
martin-call-learning <martin@call-learning.fr>
2023-07-20 12:39:15 +0200

    Added all EDOF fields to schema

--- src/Entity/Formation.php
+++ src/Entity/Formation.php
@@ -20,6 +20,24 @@ class Formation
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
+    #[ORM\Column(length: 6)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $globalGoal = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $pedaGoal = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $content = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $highlights = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $expectedResults = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -45,6 +63,78 @@ class Formation
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getGlobalGoal(): ?string
+    {
+        return $this->globalGoal;
+    }
+
+    public function setGlobalGoal(?string $globalGoal): self
+    {
+        $this->globalGoal = $globalGoal;
+
+        return $this;
+    }
+
+    public function getPedaGoal(): ?string
+    {
+        return $this->pedaGoal;
+    }
+
+    public function setPedaGoal(?string $pedaGoal): self
+    {
+        $this->pedaGoal = $pedaGoal;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getHighlights(): ?string
+    {
+        return $this->highlights;
+    }
+
+    public function setHighlights(?string $highlights): self
+    {
+        $this->highlights = $highlights;
+
+        return $this;
+    }
+
+    public function getExpectedResults(): ?string
+    {
+        return $this->expectedResults;
+    }
+
+    public function setExpectedResults(?string $expectedResults): self
+    {
+        $this->expectedResults = $expectedResults;
 
         return $this;
     }
